86ed6504dda2a2d09cf0c23cf6ae9e2501261eb3
David Daza <03dazal@gmail.com>
2020-12-04 10:56:16 -0500

    first homepage render

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,23 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink: '#d8277c',
+        onyx: '#3c3f47',
+        purple: '#6451a0',
+        mnblue: '#3e5588',
+        raisin: '#2a2e3a',
+        cultured: '#f5f5f5',
+      },
+      fontFamily: {
+        grotesk: ['"Neue Haas Grotesk"', 'sans-serif'],
+        open: ['"Open Sans"', 'sanss-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
