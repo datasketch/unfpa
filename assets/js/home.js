ce89f24ac886285857fbcc33a68e7b0a3341fccb
David Daza <03dazal@gmail.com>
2020-12-04 13:35:30 -0500

    add explorer carousel

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,14 +1,28 @@
-const mythSwiper = new Swiper('.myths', {
-  slidesPerView: 1,
+const explorerSwiper = new Swiper('.explorer', {
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination.explorer-pagination',
     clickable: true,
     renderBullet: function (_, className) {
-      return `<span class="${className} myth-bullet"></span>`;
+      return `<span class="${className} custom-bullet"></span>`;
     },
   },
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next.explorer-navigation',
+    prevEl: '.swiper-button-prev.explorer-navigation',
+  },
+});
+
+const mythSwiper = new Swiper('.myths', {
+  pagination: {
+    el: '.swiper-pagination.myths-pagination',
+    clickable: true,
+    renderBullet: function (_, className) {
+      return `<span class="${className} custom-bullet"></span>`;
+    },
+  },
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next.myths-navigation',
+    prevEl: '.swiper-button-prev.myths-navigation',
   },
 });
